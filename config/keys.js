23711cf7b5d8affc8cb56b48956423876d66f608
Roshan Raj <roshan139154@gmail.com>
2019-11-05 21:50:58 +0530

    Added Cloud Database

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   // mongoURI: "mongodb://localhost:27017/trainee-system",
-  mongoURI: "mongodb://root:root12345>@ds149596.mlab.com:49596/trainee-system",
+  mongoURI: "mongodb://root:root12345@ds149596.mlab.com:49596/trainee-system",
   secretOrKey: "secret"
 };
